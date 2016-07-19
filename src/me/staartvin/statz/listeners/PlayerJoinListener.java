d5dc47c5c3161f3c16b69279a85d92a523f23da5
Staartvin <mijnleraar@msn.com>
2016-07-19 12:12:30 +0200

    Run PlayerJoinEvent async

--- src/me/staartvin/statz/listeners/PlayerJoinListener.java
+++ src/me/staartvin/statz/listeners/PlayerJoinListener.java
@@ -23,55 +23,67 @@ public class PlayerJoinListener implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onJoin(final PlayerJoinEvent event) {
 
-		final PlayerStat stat = PlayerStat.JOINS;
+		plugin.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
 
-		// Get player
-		final Player player = event.getPlayer();
+			public void run() {
+				final PlayerStat stat = PlayerStat.JOINS;
 
-		// Update name in database.
-		plugin.getDataManager().setPlayerInfo(player.getUniqueId(), PlayerStat.PLAYERS, StatzUtil.makeQuery("uuid", player.getUniqueId().toString(), "playerName", player.getName()));
+				// Get player
+				final Player player = event.getPlayer();
 
-		// Get player info.
-		final PlayerInfo info = plugin.getDataManager().getPlayerInfo(player.getUniqueId(), stat);
+				// Update name in database.
+				plugin.getDataManager().setPlayerInfo(player.getUniqueId(), PlayerStat.PLAYERS,
+						StatzUtil.makeQuery("uuid", player.getUniqueId().toString(), "playerName", player.getName()));
 
-		// Get current value of stat.
-		double currentValue = 0;
-
-		// Check if it is valid!
-		if (info.isValid()) {
-			currentValue = info.getTotalValue();
-		}
-
-		// Update value to new stat.
-		plugin.getDataManager().setPlayerInfo(player.getUniqueId(), stat,
-				StatzUtil.makeQuery("uuid", player.getUniqueId().toString(), "value", (currentValue + 1)));
-		
-		// Player has joined, so create a timer that runs every minute to add time.
-		BukkitRunnable run = new BukkitRunnable() {
-			public void run(){
-				if (!player.isOnline()) {
-					this.cancel();
-					return;
-				}
-				
 				// Get player info.
-				final PlayerInfo info = plugin.getDataManager().getPlayerInfo(player.getUniqueId(), PlayerStat.TIME_PLAYED, StatzUtil.makeQuery("world", player.getWorld().getName()));
+				final PlayerInfo info = plugin.getDataManager().getPlayerInfo(player.getUniqueId(), stat);
 
 				// Get current value of stat.
 				double currentValue = 0;
 
 				// Check if it is valid!
 				if (info.isValid()) {
-					currentValue += info.getTotalValue();
+					currentValue = info.getTotalValue();
 				}
 
 				// Update value to new stat.
-				plugin.getDataManager().setPlayerInfo(player.getUniqueId(), PlayerStat.TIME_PLAYED,
-						StatzUtil.makeQuery("uuid", player.getUniqueId().toString(), "value", (currentValue + 1), "world", player.getWorld().getName()));
-				
+				plugin.getDataManager().setPlayerInfo(player.getUniqueId(), stat,
+						StatzUtil.makeQuery("uuid", player.getUniqueId().toString(), "value", (currentValue + 1)));
+
+				// Player has joined, so create a timer that runs every minute to add time.
+				BukkitRunnable run = new BukkitRunnable() {
+					public void run() {
+						if (!player.isOnline()) {
+							this.cancel();
+							return;
+						}
+
+						// Get player info.
+						final PlayerInfo info = plugin.getDataManager().getPlayerInfo(player.getUniqueId(),
+								PlayerStat.TIME_PLAYED, StatzUtil.makeQuery("world", player.getWorld().getName()));
+
+						// Get current value of stat.
+						double currentValue = 0;
+
+						// Check if it is valid!
+						if (info.isValid()) {
+							currentValue += info.getTotalValue();
+						}
+
+						// Update value to new stat.
+						plugin.getDataManager().setPlayerInfo(player.getUniqueId(), PlayerStat.TIME_PLAYED,
+								StatzUtil.makeQuery("uuid", player.getUniqueId().toString(), "value",
+										(currentValue + 1), "world", player.getWorld().getName()));
+
+					}
+				};
+
+				run.runTaskTimer(plugin,
+						(currentValue == 0 ? 0
+								: 20 * 60) /*If currentValue is 0, schedule a check immediately, otherwise after a minute*/,
+						20 * 60 /*Every minute*/);
 			}
-		};
-		
-		run.runTaskTimer(plugin, (currentValue == 0 ? 0 : 20*60) /*If currentValue is 0, schedule a check immediately, otherwise after a minute*/, 20 * 60 /*Every minute*/);
+
+		});
 	}
 }
