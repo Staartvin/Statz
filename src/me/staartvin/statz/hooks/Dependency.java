993c54c21f2f946f52bf7e12b75eb8141d2de19d
Staartvin <mijnleraar@msn.com>
2017-07-25 19:49:50 +0200

    Remove uSkyBlock handler as PluginLibrary handles it now.

--- src/me/staartvin/statz/hooks/Dependency.java
+++ src/me/staartvin/statz/hooks/Dependency.java
@@ -6,14 +6,11 @@ import org.bukkit.Bukkit;
 import me.staartvin.statz.Statz;
 import me.staartvin.statz.hooks.handlers.NuVotifierHandler;
 import me.staartvin.statz.hooks.handlers.VotifierHandler;
-import me.staartvin.statz.hooks.handlers.uSkyBlockHandler;
-import org.royaldev.royalcommands.VaultHandler;
 
 public enum Dependency {
 
     VOTIFIER("Votifier", new VotifierHandler()),
     NUVOTIFIER("Votifier", new NuVotifierHandler()),
-    USKYBLOCK("uSkyBlock", new uSkyBlockHandler()),
     PLUGINLIBRARY("PluginLibrary", new PluginLibraryHandler());
 
     Dependency(String internalName) {
